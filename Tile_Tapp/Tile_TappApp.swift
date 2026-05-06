833dc26c94e07c07e0912a2892c9791718317acb
Shrirkishba <123809369+Krishna7420@users.noreply.github.com>
2026-05-06 11:25:07 +0530

    Initial Commit

---
+++ Tile_Tapp/Tile_TappApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tile_TappApp.swift
+//  Tile_Tapp
+//
+//  Created by Shrikrishna Thodsare on 06/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Tile_TappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
